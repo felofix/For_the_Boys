63ffba047310e4d5ad0c1cd5b9efc3ac48916e7b
Erik <erik@localhost.localdomain>
2022-03-03 18:51:42 +0100

    skeleton for tests

---
+++ tests/all_tests.sh
@@ -0,0 +1,3 @@
+python audio_unittest.py
+python video_unittest.py
+python integration_test.py
---
+++ tests/audio_unittest.py
@@ -0,0 +1,21 @@
+import unittest
+
+class TestAudio(unittest.TestCase):
+
+    def test_invalid_inputs(self):
+        pass
+
+    def test_encoding(self):
+        pass
+
+    def test_decoding(self):
+        pass
+
+    def test_efficiency(self):
+        pass
+
+    #def test_negere
+
+
+if __name__ == "__main__":
+    unittest.main()
